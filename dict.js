b3f74d233deba9a62ea8ad3a727294106e5ed155
Peixin Liu <fengyi.mail@gmail.com>
2016-06-02 14:58:15 +0800

    add dict to look up word

---
+++ dict.js
@@ -0,0 +1,90 @@
+#!/usr/local/bin/node
+
+var request = require('request');
+var Q = require('q');
+var _ = require('lodash');
+var exec = require('child_process').exec;
+var os = require('os');
+
+var srcLan = 'en';
+var dstLan = 'zh';
+
+var sayIt = false
+
+function getExplain(srcString) {
+	var deferred = Q.defer();
+	request.post({
+		url: 'http://fanyi.baidu.com/transapi',
+		form: {
+			from: srcLan,
+			to: dstLan,
+			query: srcString,
+			source: 'txt',
+		}
+	}, function(err, httpResponse, body) {
+		response = JSON.parse(body);
+		if (err || response.status != 0)
+			deferred.reject(err || response.status);
+
+		else if (response.hasOwnProperty('data'))
+			deferred.resolve(['data', response.data[0]]);
+		else
+			deferred.resolve(['result', JSON.parse(response.result)]);
+	}, function(err, status) {
+		deferred.reject(err);
+	})
+	return deferred.promise;
+}
+
+function showResult(type, result) {
+	console.log('----------------------------------');
+	console.log(result.src);
+	if (type == 'data') {
+		console.log();
+		console.log(result.dst);
+		return
+	}
+	phonic = ''
+	_.forEach(result.voice, function(obj) {
+		phonicObj = _.toPairs(obj)[0];
+		phonicObj[0] = phonicObj[0].split('_')[0];
+		phonic += _.join(phonicObj, ': ');
+		phonic += '  ';
+	});
+	console.log(phonic);
+	console.log();
+	_.forEach(result.content[0].mean, function(mean) {
+		console.log(mean.pre + '  ' + _.keys(mean.cont).join('; '));
+	})
+	console.log('----------------------------------');
+	sayWords(result.src);
+}
+
+function doTranslate(srcString) {
+	getExplain(srcString).then(
+		function(result) {
+			showResult(result[0], result[1]);
+		},
+		function() {
+			console.log('invalid dst language: ' + dstLan);
+		}
+	);
+}
+
+function sayWords(srcString) {
+	if (sayIt && os.type() == 'Darwin')
+		exec('say ' + srcString);
+}
+
+function run() {
+	if (process.argv.length >= 3) {
+		srcString = process.argv[2];
+		doTranslate(srcString);
+	} else {
+		console.log('usage: ./cn2tw.js dest_string');
+	}
+}
+
+if (require.main === module) {
+	run();
+}
--- translate.js
+++ translate.js
@@ -1,34 +1,33 @@
 #!/usr/local/bin/node
 
 var request = require('request');
-var Q   = require('q');
+var Q = require('q');
 var srcLan = 'zh';
 var dstLan = 'cht';
 
 function detectLanguage(srcString) {
 	var deferred = Q.defer();
-	var onSuccess = function(err, httpResponse, body){
+	var onSuccess = function(err, httpResponse, body) {
 		response = JSON.parse(body);
-		if(err || response.error != 0)
-		{
-			return deferred.reject(error || response.error)
+		if (err || response.error != 0) {
+			return deferred.reject(error || response.error);
 		}
-		return deferred.resolve(response.lan)
+		return deferred.resolve(response.lan);
 	}
-	var onError = function(err, status){
-		return deferred.reject(err)
+	var onError = function(err, status) {
+		return deferred.reject(err);
 	}
 
 	request.post({
-		url: 'http://fanyi.baidu.com/langdetect',
-		form : {
-			query: srcString
-		}
-	},
-	onSuccess,
-	onError
+			url: 'http://fanyi.baidu.com/langdetect',
+			form: {
+				query: srcString
+			}
+		},
+		onSuccess,
+		onError
 	)
-	return deferred.promise
+	return deferred.promise;
 }
 
 function translate(srcString) {
@@ -38,33 +37,33 @@ function translate(srcString) {
 		form: {
 			from: srcLan,
 			to: dstLan,
-			query:srcString,
+			query: srcString,
 			transtype: 'translang',
 			simple_means_flag: '3'
 		}
 	}, function(err, httpResponse, body) {
 		response = JSON.parse(body);
 		if (err || response.error)
-			deferred.reject(err || response.error)
+			deferred.reject(err || response.error);
 
 		else
 			deferred.resolve(response.trans_result.data[0].dst);
 	}, function(err, status) {
-		deferred.reject(err)
+		deferred.reject(err);
 	})
-	return deferred.promise
+	return deferred.promise;
 }
 
 function toSBC(tempString) {
 	result = '';
 	len = tempString.length;
-	for (var i=0; i < len; i++) {
+	for (var i = 0; i < len; i++) {
 		var cCode = tempString.charCodeAt(i);
 		cCode = (cCode >= 0xFF01 && cCode <= 0xFF5E) ? cCode - 0xFEE0 : cCode;
-		cCode = (cCode==0x03000)?0x0020:cCode;
+		cCode = (cCode == 0x03000) ? 0x0020 : cCode;
 		result += String.fromCharCode(cCode);
 	}
-	return result
+	return result;
 }
 
 function showResult(src, dst) {
@@ -76,31 +75,36 @@ function showResult(src, dst) {
 }
 
 function doTranslate() {
-	translate(srcString).then (
+	translate(srcString).then(
 		function(dstString) {
-			dstString = toSBC(dstString)
-			showResult(srcString, dstString)
-		}
-		, function () {
-			console.log('invalid dst language: ' + dstLan)
+			dstString = toSBC(dstString);
+			showResult(srcString, dstString);
+		},
+		function() {
+			console.log('invalid dst language: ' + dstLan);
 		}
 	);
 }
 
-if (process.argv.length >= 3) {
-	srcString = process.argv[2]	
-	if (process.argv.length == 4) {
-		dstLan = process.argv[3];
+function run() {
+	if (process.argv.length >= 3) {
+		srcString = process.argv[2]
+		if (process.argv.length == 4) {
+			dstLan = process.argv[3];
+		}
+		detectLanguage(srcString).then(
+			function(language) {
+				srcLan = language;
+				doTranslate();
+			},
+			doTranslate
+		);
+
+	} else {
+		console.log('usage: ./cn2tw.js src_string [dest_language: en/cht/...]');
 	}
-	detectLanguage(srcString).then(
-		function(language){ 
-			srcLan = language;
-			doTranslate();
-		},
-		doTranslate
-	);	
-	
 }
-else {
-	console.log('usage: ./cn2tw.js src_string [dest_language: en/cht/...]');
+
+if (require.main === module) {
+	run();
 }
